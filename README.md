9172436aaca9d7bc935f03f0220d4a2aa2ba5ec2
Manuel Cerezo Bonilla <manueldevjour@gmail.com>
2020-02-27 16:03:29 +0100

    README and first exercise
    - README updated with official documentation
    - First and basic exercise in Python
    
    WIP

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # practicing-python
+
 Learning Python with some exercises
+
+**[Python official tutorial](https://docs.python.org/3/tutorial/)**
---
+++ basic_exercises/output-text.py
@@ -0,0 +1,5 @@
+#Basic exercise working with boolean, if conditional and print
+
+hello_world = True
+if hello_world:
+    print("Hello World! I'm here.")
