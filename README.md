be085efd7d3595ec6e1c0d4febda31976dd664dc
Manuel Cerezo Bonilla <manueldevjour@gmail.com>
2020-02-29 21:16:18 +0100

    Update README and variance.py
    - README updated for giving links to statistics post
    - variance.py must be tested (Python3 not working on my Windows :) )

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Learning Python with some exercises
 
 **[Python official tutorial](https://docs.python.org/3/tutorial/)**
+
+
+### Statistics
+
+In this folder, I'll do some exercises related with **[this explanation](https://relopezbriega.github.io/blog/2015/06/27/probabilidad-y-estadistica-con-python/)**
--- statistics/variance.py
+++ statistics/variance.py
@@ -1,5 +1,8 @@
+# Here we are going to calculate the variance of a multidimensional array
+
 import numpy as np
-datos = np.array([
+
+data = np.array([
 [0,1,2,3,4,5,6,7,8,9],
 [2,3,2,3,4,5,6,7,6,7],
 [2,3,2,3,4,5,6,7,8,9],
@@ -9,9 +12,10 @@ datos = np.array([
 [5,5,6,6,7,7,8,8,9,9],
 [1,1,2,2,3,3,4,4,5,5]
 ])
-for dato in datos:
-    print('Array: ', dato)
-    print('Media: ', dato.mean())
-    print('Desvi: ', np.std(dato))
-    print('Varia: ', np.var(dato))
+for d in data:
+    print('Array: ', d)
+    print('Mean: ', d.mean())
+    print('Deviation: ', np.std(d))
+    print('Variance: ', np.var(d))
+    print('mean of each column: ', np.mean(axis=0))
     print(' ')
